368e578c1e96d59bbbbca5fd1148cdbeef8b6e04
Igor <igor.kh.mail@gmail.com>
2014-09-03 18:44:24 +0300

    query builders to search flight and airports fixed

--- JetStreamCommons/JetStreamCommons/Helpers/QueryHelpers.cs
+++ JetStreamCommons/JetStreamCommons/Helpers/QueryHelpers.cs
@@ -8,21 +8,32 @@ namespace JetStreamCommons
     {
     }
 
-    public static string QueryToGetAllFlights()
+    public static string QueryToSearchAirportsWithName(string name)
     {
-      return "/sitecore/content/Global/Airports//*";
+      return "/sitecore/content/Global/Airports//*[@@templatename='Airport' and contains(@Airport Name, '" + name + "')]";
     }
 
-    public static string QueryToSearchTicketsWithRequest(SearchTicketsRequest request)
+    public static string QueryToSearchDepartFlightsWithRequest(SearchFlightsRequest request)
     {
       return "query=sitecore/content/Global/Flights//*" +
         "[" +
         "@@templatename='Flight' " +
-        "and contains(@Departure Airport, '{2F8696E9-150B-43B1-B08B-EEEF82683B67}') " +
-        "and contains(@Arrival Airport, '{4961A465-7684-4C9F-A9F8-C63ACBE778C0}') " +
+        "and contains(@Departure Airport, '" + request.FromAirportId + "') " +
+        "and contains(@Arrival Airport, '" + request.ToAirportId + "') " +
         //TODO: date to string convertion
-        "and contains(@Departure Time, '" +request.FlightDate.ToString()+ "') " +
-       
+        "and contains(@Departure Time, '" + request.DepartDate.ToString() + "') " +
+        "]";
+    }
+
+    public static string QueryToSearchReturnFlightsWithRequest(SearchFlightsRequest request)
+    {
+      return "query=sitecore/content/Global/Flights//*" +
+        "[" +
+        "@@templatename='Flight' " +
+        "and contains(@Departure Airport, '" + request.ToAirportId + "') " +
+        "and contains(@Arrival Airport, '" + request.FromAirportId + "') " +
+        //TODO: date to string convertion
+        "and contains(@Departure Time, '" + request.ReturnDate.ToString() + "') " +
         "]";
     }
   }
