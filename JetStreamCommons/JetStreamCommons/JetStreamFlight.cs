02525821a80d2a8b2849a3ec5726b28dcd37fba3
Igor <igor.kh.mail@gmail.com>
2014-09-03 18:43:36 +0300

    objects to proceed flight search added

--- JetStreamCommons/JetStreamCommons/JetStreamFlight.cs
+++ JetStreamCommons/JetStreamCommons/JetStreamFlight.cs
@@ -1,19 +1,76 @@
 ﻿using System;
 using Sitecore.MobileSDK.Items;
+
 namespace JetStreamCommons
 {
-  public class JetStreamTicket
+  public class JetStreamFlight
   {
-    public JetStreamTicket()
+    public JetStreamFlight(ScItem flight)
     {
+      this.Flight = flight;
     }
 
-    public ScItem Airport
+    private ScItem Flight
     {
       get;
-      private set;
+      set;
     }
 
+    public string DepartureAirportId
+    {
+      get
+      {
+        return this.Flight.FieldWithName ("Departure Airport").RawValue;
+      }
+    }
+
+    public string ArrivalAirportId
+    {
+      get
+      {
+        return this.Flight.FieldWithName ("Arrival Airport").RawValue;
+      }
+    }
+
+    public string DepartureDate
+    {
+      get
+      {
+        return this.Flight.FieldWithName ("Departure Time").RawValue;
+      }
+    }
+
+    public string DeparturePrice
+    {
+      get
+      {
+        return this.Flight.FieldWithName ("Price").RawValue;
+      }
+    }
+
+    public string DeparturePersonalEntertainment
+    {
+      get
+      {
+        return this.Flight.FieldWithName ("Personal Entertainment").RawValue;
+      }
+    }
+
+    public string DepartureInFlightWifi 
+    {
+      get
+      {
+        return this.Flight.FieldWithName ("In Flight Wifi").RawValue;
+      }
+    }
+
+    public string DepartureFoodService
+    {
+      get
+      {
+        return this.Flight.FieldWithName ("Food Service").RawValue;
+      }
+    }
 
   }
 }
--- JetStreamCommons/JetStreamCommons/JetStreamOrder.cs
+++ JetStreamCommons/JetStreamCommons/JetStreamOrder.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
+using Sitecore.MobileSDK.Items;
 namespace JetStreamCommons
 {
   public class JetStreamOrder
   {
-    public JetStreamOrder()
+    public JetStreamOrder(JetStreamFlight departFlight, JetStreamFlight returnFlight)
     {
+      this.DepartFlight = departFlight;
+      this.ReturnFlight = returnFlight;
     }
+
+    private JetStreamFlight DepartFlight
+    {
+      get;
+      set;
+    }
+
+    private JetStreamFlight ReturnFlight
+    {
+      get;
+      set;
+    }
+
   }
 }
 
