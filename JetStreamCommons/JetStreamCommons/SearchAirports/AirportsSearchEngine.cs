06357627ebbf92450b5d1e13d600447cf7d556f7
dodikk <dodikk88.reg@gmail.com>
2014-09-09 14:08:55 +0300

    [airports] Added a search filter class

---
+++ JetStreamCommons/JetStreamCommons/SearchAirports/AirportsSearchEngine.cs
@@ -0,0 +1,14 @@
+﻿namespace JetStreamCommons.SearchAirports
+{
+  public class AirportsSearchEngine
+  {
+    private textToSearch;
+
+    public AirportsSearchEngine(string textToSearch)
+    {
+    }
+
+
+  }
+}
+
