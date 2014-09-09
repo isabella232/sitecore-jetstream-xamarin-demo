b26087931bd184ead4b74ab3e5566f2d5ad022a5
Igor <igor.kh.mail@gmail.com>
2014-09-09 15:43:56 +0300

    checking if data correct before request will be built

--- JetStreamCommons/JetStreamCommons/SearchTickets/SearchTicketsRequestBuilder.cs
+++ JetStreamCommons/JetStreamCommons/SearchTickets/SearchTicketsRequestBuilder.cs
@@ -10,43 +10,81 @@ namespace JetStreamCommons
 
     public SearchTicketsRequestBuilder SetFromAirportItem(string airportId)
     {
-      this.FromAirportId = airportId;
+      this.fromAirportId = airportId;
       return this;
     }
 
     public SearchTicketsRequestBuilder SetToAirportItem(string airportId)
     {
-      this.ToAirportId = airportId;
+      this.toAirportId = airportId;
       return this;
     }
 
     public SearchTicketsRequestBuilder SetDepartDate(DateTime date)
     {
-      this.DepartDate = date;
+      this.departDate = date;
       return this;
     }
 
     public SearchTicketsRequestBuilder SetReturnDate(DateTime date)
     {
-      this.ReturnDate = date;
+      this.returnDate = date;
       return this;
     }
 
     public SearchFlightsRequest Build()
     {
+      this.CheckRequestDataWithExeption();
+
       SearchFlightsRequest request = new SearchFlightsRequest (
-                                        this.FromAirportId,
-                                        this.ToAirportId,
-                                        this.DepartDate,
-                                        this.ReturnDate
+                                        this.fromAirportId,
+                                        this.toAirportId,
+                                        this.departDate,
+                                        this.returnDate
                                      );
       return request;
     }
 
-    private string FromAirportId;
-    private string ToAirportId;
-    private DateTime DepartDate;
-    private DateTime ReturnDate;
+    private void CheckRequestDataWithExeption()
+    {
+      if (null == this.fromAirportId)
+      {
+        throw new ArgumentNullException("FROM_AIRPORT_IS_NULL");
+      }
+
+      if (null == this.toAirportId)
+      {
+        throw new ArgumentNullException("TO_AIRPORT_IS_NULL");
+      }
+
+      if (null == this.departDate)
+      {
+        throw new ArgumentNullException("DEPART_DATE_IS_NULL");
+      }
+
+      DateTime pastDate = DateTime.Now;
+      pastDate =pastDate.AddDays (-1);
+
+      if (pastDate > this.departDate)
+      {
+        throw new ArgumentNullException("DEPART_DATE_MUST_BE_A_FUTURE");
+      }
+
+      if (pastDate > this.returnDate)
+      {
+        throw new ArgumentNullException("RETURN_DATE_MUST_BE_A_FUTURE");
+      }
+
+      if (departDate > returnDate)
+      {
+        throw new ArgumentException("RETURN_DATE_MUST_BE_AFTER_DEPARTURE");
+      }
+    }
+
+    private string fromAirportId;
+    private string toAirportId;
+    private DateTime departDate;
+    private DateTime returnDate;
   }
 }
 
