b29716ad49ea90359b1aea8a5a3f5b8e9ac7701d
dodikk <dodikk88.reg@gmail.com>
2014-09-10 20:15:33 +0300

    [flight] Added a convertor for boolean raw values

---
+++ JetStreamCommons/JetStreamCommons/Helpers/RawValueConverter.cs
@@ -0,0 +1,37 @@
+﻿namespace JetStreamCommons
+{
+  using System;
+
+
+  public static class RawValueConverter
+  {
+    public static bool ToBoolean(string rawValue)
+    {
+      if (string.IsNullOrWhiteSpace(rawValue))
+      {
+        string message = "RawValue does not have a boolean value because it is empty.";
+        throw new ArgumentException(message);
+      }
+
+      const string strTrue = "true";
+      const string strFalse = "false";
+
+      string lowerCaseInput = rawValue.ToLowerInvariant();
+
+      if (strTrue.Equals(lowerCaseInput))
+      {
+        return true;
+      }
+      else if (strFalse.Equals(lowerCaseInput))
+      {
+        return false;
+      }
+      else
+      {
+        string message = string.Format("RawValue does not have a boolean value. Actual value : |{0}| ", rawValue);
+        throw new ArgumentException(message);
+      }
+    }
+  }
+}
+
