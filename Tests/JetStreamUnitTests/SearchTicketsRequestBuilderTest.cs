8e51e06b54b83f49b0248a55cf0f53e944ecf75a
Igor <igor.kh.mail@gmail.com>
2014-09-10 18:50:47 +0300

    iOS unit test project added

---
+++ Tests/JetStreamUnitTests/SearchTicketsRequestBuilderTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace JetStreamUnitTests
+{
+  [TestFixture ()]
+  public class SearchTicketsRequestBuilderTest
+  {
+    [Test ()]
+    public void TestCase()
+    {
+    }
+  }
+}
+
