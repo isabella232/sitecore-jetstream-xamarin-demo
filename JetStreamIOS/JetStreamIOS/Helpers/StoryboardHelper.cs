8734862a35c17fdcde99f060fde89eeefd15b2e4
dodikk <dodikk88.reg@gmail.com>
2014-09-11 15:48:22 +0300

    [flights] Extracted storyboard constants

---
+++ JetStreamIOS/JetStreamIOS/Helpers/StoryboardHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace JetStreamIOS.Helpers
+{
+  using System;
+  using MonoTouch.UIKit;
+
+
+  public static class StoryboardHelper
+  {
+    public static bool IsSegueToSourceAirportSearch(UIStoryboardSegue segue)
+    {
+      return "FromAirportQuickSearch".Equals(segue.Identifier);
+    }
+
+    public static bool IsSegueToDestinationAirportSearch(UIStoryboardSegue segue)
+    {
+      return "ToAirportQuickSearch".Equals(segue.Identifier);
+    }
+
+    public static bool IsSegueToDepartureFlightsSearch(UIStoryboardSegue segue)
+    {
+      return "ToDepartureFlightsList".Equals(segue.Identifier);
+    }
+  }
+}
+
