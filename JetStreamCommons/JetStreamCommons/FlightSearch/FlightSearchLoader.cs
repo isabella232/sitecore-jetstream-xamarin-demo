212e7b76c3c7ca2b0dd836f42806c27c7b1f154d
dodikk <dodikk88.reg@gmail.com>
2014-09-11 14:46:24 +0300

    [flights] Added stub methods

--- JetStreamCommons/JetStreamCommons/FlightSearch/FlightSearchLoader.cs
+++ JetStreamCommons/JetStreamCommons/FlightSearch/FlightSearchLoader.cs
@@ -1,9 +1,13 @@
-﻿namespace JetStreamCommons.FlightSearch
+﻿
+
+namespace JetStreamCommons.FlightSearch
 {
   using System;
+  using System.Threading.Tasks;
   using System.Collections.Generic;
 
   using JetStreamCommons;
+  using JetStreamCommons.Flight;
   using JetStreamCommons.Airport;
 
 
@@ -14,9 +18,23 @@
       IJetStreamAirport sourceAirport,
       IJetStreamAirport destinationAirport,
       DateTime flightDate,
-      int datePrecisionInDays
-    )
+      int datePrecisionInDays)
     {
+    }
+
+    public async Task< IEnumerable<IJetStreamFlight> > GetFlightsForTheGivenDateAsync()
+    {
+      return null;
+    }
+
+    public async Task<DaySummary> GetPreviousDayAsync()
+    {
+      return null;
+    }
+
+    public async Task<DaySummary> GetNextDayAsync()
+    {
+      return null;
     }
   }
 }
