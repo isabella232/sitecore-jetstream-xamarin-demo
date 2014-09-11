0b311541644310165a1c3d31d709b6513e4c78f2
dodikk <dodikk88.reg@gmail.com>
2014-09-11 18:02:29 +0300

    [flights] Added adjacent days fetching

--- JetStreamIOS/JetStreamIOS/FlightListViewController.cs
+++ JetStreamIOS/JetStreamIOS/FlightListViewController.cs
@@ -1,17 +1,93 @@
-using System;
-
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
-using JetStreamCommons.FlightSearch;
-
 namespace JetStreamIOS
 {
+  using System;
+  using System.Collections.Generic;
+
+  using MonoTouch.Foundation;
+  using MonoTouch.UIKit;
+
+  using JetStreamCommons;
+  using JetStreamCommons.Flight;
+  using JetStreamCommons.FlightSearch;
+  using Sitecore.MobileSDK.API.Session;
+
+
 	public partial class FlightListViewController : UIViewController
 	{
-		public FlightListViewController (IntPtr handle) : base (handle)
+    #region Injected Variables
+    private bool IsFlyingBack { get; set; }
+    public IFlightSearchUserInput SearchOptionsFromUser { get; set; }
+    #endregion Injected Variables
+
+    #region UIViewController
+		public FlightListViewController(IntPtr handle) : base (handle)
 		{
 		}
 
-    public IFlightSearchUserInput SearchOptionsFromUser { get; set; }
+    public override void ViewWillAppear(bool animated)
+    {
+      DateTime today = this.SearchOptionsFromUser.ForwardFlightDepartureDate;
+      DateTime yesterday = today.AddDays(-1);
+      DateTime tomorrow  = today.AddDays(+1);
+
+      this.TodayDateLabel.Text = DateConverter.StringFromDateForUI(today);
+      this.YesterdayDateLabel.Text = DateConverter.StringFromDateForUI(yesterday);
+      this.TomorrowDateLabel.Text = DateConverter.StringFromDateForUI(tomorrow);
+
+      this.YesterdayPriceLabel.Text = NSBundle.MainBundle.LocalizedString("PRICE_UNAVAILABLE", null);
+      this.TomorrowPriceLabel.Text = NSBundle.MainBundle.LocalizedString("PRICE_UNAVAILABLE", null);
+
+      this.ReloadData();
+    }
+    #endregion UIViewController
+
+    #region IBAction
+    partial void OnTomorrowButtonPressed (MonoTouch.Foundation.NSObject sender)
+    {
+      AlertHelper.ShowLocalizedAlertWithOkOption("tomorrow", "tomorrow");
+    }
+
+    partial void OnYesterdayButtonPressed (MonoTouch.Foundation.NSObject sender)
+    {
+      AlertHelper.ShowLocalizedAlertWithOkOption("yesterday", "yesterday");
+    }
+    #endregion IBAction
+
+
+    private async void ReloadData()
+    {
+      // @adk : on top of NSUserDefaults singleton
+      InstanceSettings endpoint = new InstanceSettings();
+      ISitecoreWebApiSession webApiSession = endpoint.GetSession();
+      using (RestManager jetStreamSession = new RestManager(webApiSession))
+      {
+        var loader = new FlightSearchLoader(
+          jetStreamSession,
+          this.SearchOptionsFromUser.SourceAirport,
+          this.SearchOptionsFromUser.DestinationAirport,
+          this.SearchOptionsFromUser.ForwardFlightDepartureDate);
+
+        IEnumerable<IJetStreamFlight> flights = await loader.GetFlightsForTheGivenDateAsync();
+        DaySummary yesterday = await loader.GetPreviousDayAsync();
+        DaySummary tomorrow = await loader.GetNextDayAsync();
+
+        this.YesterdayDateLabel.Text = DateConverter.StringFromDateForUI(yesterday.DepartureDate);
+
+
+        this.YesterdayPriceLabel.Text = yesterday.LowestPrice.HasValue ? 
+            yesterday.LowestPrice.Value.ToString("C") : 
+            NSBundle.MainBundle.LocalizedString("PRICE_UNAVAILABLE", null);
+
+        this.TomorrowDateLabel.Text = DateConverter.StringFromDateForUI(tomorrow.DepartureDate);
+        this.TomorrowPriceLabel.Text = tomorrow.LowestPrice.HasValue ? 
+          tomorrow.LowestPrice.Value.ToString("C") :
+          NSBundle.MainBundle.LocalizedString("PRICE_UNAVAILABLE", null);
+
+      }
+
+      // TODO : 
+    }
+
+
 	}
 }
