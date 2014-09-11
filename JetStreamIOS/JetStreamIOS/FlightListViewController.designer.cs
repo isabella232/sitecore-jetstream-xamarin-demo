dcd03abbb75b8157cf1167ca05b3bc6761afc6c3
dodikk <dodikk88.reg@gmail.com>
2014-09-11 18:00:56 +0300

    [flights] Added GUI

--- JetStreamIOS/JetStreamIOS/FlightListViewController.designer.cs
+++ JetStreamIOS/JetStreamIOS/FlightListViewController.designer.cs
@@ -12,9 +12,61 @@ namespace JetStreamIOS
 	[Register ("FlightListViewController")]
 	partial class FlightListViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UITableView FlightsTableView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel TodayDateLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel TomorrowDateLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel TomorrowPriceLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel YesterdayDateLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel YesterdayPriceLabel { get; set; }
+
+		[Action ("OnTomorrowButtonPressed:")]
+		partial void OnTomorrowButtonPressed (MonoTouch.Foundation.NSObject sender);
+
+		[Action ("OnYesterdayButtonPressed:")]
+		partial void OnYesterdayButtonPressed (MonoTouch.Foundation.NSObject sender);
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (FlightsTableView != null) {
+				FlightsTableView.Dispose ();
+				FlightsTableView = null;
+			}
+
+			if (YesterdayPriceLabel != null) {
+				YesterdayPriceLabel.Dispose ();
+				YesterdayPriceLabel = null;
+			}
+
+			if (YesterdayDateLabel != null) {
+				YesterdayDateLabel.Dispose ();
+				YesterdayDateLabel = null;
+			}
+
+			if (TomorrowPriceLabel != null) {
+				TomorrowPriceLabel.Dispose ();
+				TomorrowPriceLabel = null;
+			}
+
+			if (TomorrowDateLabel != null) {
+				TomorrowDateLabel.Dispose ();
+				TomorrowDateLabel = null;
+			}
+
+			if (TodayDateLabel != null) {
+				TodayDateLabel.Dispose ();
+				TodayDateLabel = null;
+			}
 		}
 	}
 }
