7d5e9712e80e869f81dab237f00185be83c04c52
dodikk <dodikk88.reg@gmail.com>
2014-09-11 18:58:33 +0300

    [flight] Added a cell class

---
+++ JetStreamIOS/JetStreamIOS/ViewControllers/FlightsTable/FlightCell.designer.cs
@@ -0,0 +1,53 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace JetStreamIOS
+{
+	[Register ("FlightCell")]
+	partial class FlightCell
+	{
+		[Outlet]
+		MonoTouch.UIKit.UILabel ArrivalTimeLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel DepartureTimeLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton OrderButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel PriceLabel { get; set; }
+
+		[Action ("OrderButtonPressed:")]
+		partial void OrderButtonPressed (MonoTouch.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (PriceLabel != null) {
+				PriceLabel.Dispose ();
+				PriceLabel = null;
+			}
+
+			if (OrderButton != null) {
+				OrderButton.Dispose ();
+				OrderButton = null;
+			}
+
+			if (DepartureTimeLabel != null) {
+				DepartureTimeLabel.Dispose ();
+				DepartureTimeLabel = null;
+			}
+
+			if (ArrivalTimeLabel != null) {
+				ArrivalTimeLabel.Dispose ();
+				ArrivalTimeLabel = null;
+			}
+		}
+	}
+}
