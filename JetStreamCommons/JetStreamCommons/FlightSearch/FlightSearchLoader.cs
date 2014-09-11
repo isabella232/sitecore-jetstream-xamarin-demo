d5ad297b7305962534146769f57856c2bf6efdc6
dodikk <dodikk88.reg@gmail.com>
2014-09-11 11:16:57 +0300

    [flights] Added a class

---
+++ JetStreamCommons/JetStreamCommons/FlightSearch/FlightSearchLoader.cs
@@ -0,0 +1,18 @@
+﻿namespace JetStreamCommons.FlightSearch
+{
+  using System;
+
+  using JetStreamCommons;
+  using JetStreamCommons.Airport;
+
+
+  public class FlightSearchLoader
+  {
+    public FlightSearchLoader(
+      RestManager restManagerToConsume, 
+      IJetStreamAirport departingFrom)
+    {
+    }
+  }
+}
+
