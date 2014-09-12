b6f3f7f35ea6f50242695c51c7b6f3527c04efef
Igor <igor.kh.mail@gmail.com>
2014-09-12 18:02:03 +0300

    string with time localized format added

--- JetStreamIOS/JetStreamIOS/Helpers/DateConverter.cs
+++ JetStreamIOS/JetStreamIOS/Helpers/DateConverter.cs
@@ -11,6 +11,11 @@
       return  NSDateFormatter.ToLocalizedString(date, NSDateFormatterStyle.Short, NSDateFormatterStyle.None);
     }
 
+    public static string StringFromDateTimeForSummary(DateTime date)
+    {
+      return  NSDateFormatter.ToLocalizedString(date, NSDateFormatterStyle.Short, NSDateFormatterStyle.Short);
+    }
+
     public static string StringFromTimeForUI(DateTime date)
     {
       return  NSDateFormatter.ToLocalizedString(date, NSDateFormatterStyle.None, NSDateFormatterStyle.Short);
