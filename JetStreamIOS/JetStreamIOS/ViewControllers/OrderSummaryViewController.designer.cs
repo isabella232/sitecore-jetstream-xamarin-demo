3386b6be02a68ca4d3cb249da9c9b35e70bb269b
Igor <igor.kh.mail@gmail.com>
2014-09-12 18:02:28 +0300

    summary screen view controller code

--- JetStreamIOS/JetStreamIOS/ViewControllers/OrderSummaryViewController.designer.cs
+++ JetStreamIOS/JetStreamIOS/ViewControllers/OrderSummaryViewController.designer.cs
@@ -1,4 +1,4 @@
-// WARNING
+﻿// WARNING
 //
 // This file has been generated automatically by Xamarin Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
@@ -12,9 +12,26 @@ namespace JetStreamIOS
 	[Register ("OrderSummaryViewController")]
 	partial class OrderSummaryViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton PurchaseButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIWebView SummaryInfoWebView { get; set; }
+
+		[Action ("OnPurchaseButtonTouched:")]
+		partial void OnPurchaseButtonTouched (MonoTouch.UIKit.UIButton sender);
 		
 		void ReleaseDesignerOutlets ()
 		{
+			if (PurchaseButton != null) {
+				PurchaseButton.Dispose ();
+				PurchaseButton = null;
+			}
+
+			if (SummaryInfoWebView != null) {
+				SummaryInfoWebView.Dispose ();
+				SummaryInfoWebView = null;
+			}
 		}
 	}
 }
