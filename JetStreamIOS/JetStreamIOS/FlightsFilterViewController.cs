dd983c28c46ad4e1e4af6f0c345feb42a78b7ba5
dodikk <dodikk88.reg@gmail.com>
2014-09-15 11:24:31 +0300

    [filter] Added GUI stub

---
+++ JetStreamIOS/JetStreamIOS/FlightsFilterViewController.cs
@@ -0,0 +1,22 @@
+namespace JetStreamIOS
+{
+  using System;
+
+  using MonoTouch.Foundation;
+  using MonoTouch.UIKit;
+
+
+	public partial class FlightsFilterViewController : UIViewController
+	{
+		public FlightsFilterViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+    partial void OnDoneButtonTapped (MonoTouch.Foundation.NSObject sender)
+    {
+
+
+//      this.Unwind
+    }
+	}
+}
