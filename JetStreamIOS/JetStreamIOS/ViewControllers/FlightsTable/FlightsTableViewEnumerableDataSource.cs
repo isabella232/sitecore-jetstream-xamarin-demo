bf8541f030b2714d4b13b9b11612ea43b8dbbd16
dodikk <dodikk88.reg@gmail.com>
2014-09-15 19:43:35 +0300

    [gui] added "no flights" cell

--- JetStreamIOS/JetStreamIOS/ViewControllers/FlightsTable/FlightsTableViewEnumerableDataSource.cs
+++ JetStreamIOS/JetStreamIOS/ViewControllers/FlightsTable/FlightsTableViewEnumerableDataSource.cs
@@ -24,7 +24,12 @@ namespace JetStreamIOS.ViewControllers.FlightsTable
       this.flights = flights;
       this.onFlightSelectedCallback = onFlightSelectedCallback;
     }
-      
+     
+    private bool IsNoFlightsFound()
+    {
+      return ( 0 == this.flights.Count() );
+    }
+
     public override int NumberOfSections(UITableView tableView)
     {
       return 1;
@@ -32,10 +37,42 @@ namespace JetStreamIOS.ViewControllers.FlightsTable
 
     public override int RowsInSection(UITableView tableView, int section)
     {
-      return this.flights.Count();
+      if (this.IsNoFlightsFound())
+      {
+        return 1;
+      }
+      else
+      {
+        return this.flights.Count();
+      }
     }
 
     public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+    {
+      if (this.IsNoFlightsFound())
+      {
+        return this.GetEmptyCell(tableView, indexPath);
+      }
+      else
+      {
+        return this.GetFlightCell(tableView, indexPath);
+      }
+    }
+
+    private UITableViewCell GetEmptyCell(UITableView tableView, NSIndexPath indexPath)
+    {
+      NSString reuseIdentifier = new NSString("EmptyCell");
+      UITableViewCell newCell = tableView.DequeueReusableCell(reuseIdentifier);
+      if (null == newCell)
+      {
+        newCell = new UITableViewCell(UITableViewCellStyle.Default, reuseIdentifier);
+      }
+
+      newCell.TextLabel.Text = NSBundle.MainBundle.LocalizedString("NO_FLIGHTS_FOUND", null);
+      return newCell;
+    }
+
+    private UITableViewCell GetFlightCell(UITableView tableView, NSIndexPath indexPath)
     {
       NSString reuseIdentifier = FlightCell.StaticReuseIdentifier();
       FlightCell newCell = tableView.DequeueReusableCell(reuseIdentifier) as FlightCell;
