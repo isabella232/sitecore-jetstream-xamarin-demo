044871c660e31cb8e4fae622da6056aef3b5935b
Igor <igor.kh.mail@gmail.com>
2014-09-16 18:02:46 +0300

    required data added to order

--- JetStreamCommons/JetStreamCommons/JetStreamOrder.cs
+++ JetStreamCommons/JetStreamCommons/JetStreamOrder.cs
@@ -1,13 +1,24 @@
-﻿namespace JetStreamCommons
+﻿using JetStreamCommons.Airport;
+
+namespace JetStreamCommons
 {
   using JetStreamCommons.Flight;
 
   public class JetStreamOrder
   {
-    public JetStreamOrder(IJetStreamFlight departFlight, IJetStreamFlight returnFlight)
+    public JetStreamOrder(
+      IJetStreamFlight departFlight, 
+      IJetStreamFlight returnFlight,
+      IJetStreamAirport departureAirport, 
+      IJetStreamAirport returnAirport, 
+      int ticketsCount
+    )
     {
       this.DepartureFlight = departFlight;
       this.ReturnFlight = returnFlight;
+      this.DepartureAirport = departureAirport;
+      this.DestinationAirport = returnAirport;
+      this.TicketsCount = ticketsCount;
     }
 
     public IJetStreamFlight DepartureFlight
@@ -22,6 +33,23 @@
       private set;
     }
 
+    public IJetStreamAirport DepartureAirport
+    {
+      get;
+      private set;
+    }
+
+    public IJetStreamAirport DestinationAirport
+    {
+      get;
+      private set;
+    }
+
+    public int TicketsCount
+    {
+      get;
+      private set;
+    }
   }
 }
 
