0702f1ddf8ed4ad6589e6e62c18e85c4a1358a47
Igor <igor.kh.mail@gmail.com>
2014-09-16 18:03:56 +0300

    summary screen implementation

--- JetStreamIOS/JetStreamIOS/HtmlBuilders/IOSOrderSummaryHtmlBuilder.cs
+++ JetStreamIOS/JetStreamIOS/HtmlBuilders/IOSOrderSummaryHtmlBuilder.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using JetStreamCommons;
+using MonoTouch.Foundation;
 
 namespace JetStreamIOS
 {
-  public class IOSOrderSummaryHtmlBuilder
+  public class IOSOrderSummaryHtmlBuilder : OrderSummaryHtmlBuilder
   {
     public IOSOrderSummaryHtmlBuilder()
     {
+    }
+
+    protected override string OneWayHtmlTemlate()
+    {
+      string htmlFilePath = NSBundle.MainBundle.PathForResource("SummaryOneWayTemplate", "html");
+      string templateHtmlString = System.IO.File.ReadAllText(htmlFilePath);
+      return templateHtmlString;
+    }
+
+    protected override string RoundTripHtmlTemlate()
+    {
+      string htmlFilePath = NSBundle.MainBundle.PathForResource("SummaryWithRoundTrip", "html");
+      string templateHtmlString = System.IO.File.ReadAllText(htmlFilePath);
+      return templateHtmlString;
+    }
+
+    protected override string LocalizedStringFromDate(DateTime date)
+    {
+      return DateConverter.StringFromDateTimeForSummary(date);
     }
   }
 }
