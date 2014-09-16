a21a0a58a806c95465b9344b741c29d4ef0e7471
Igor <igor.kh.mail@gmail.com>
2014-09-16 10:10:19 +0300

    resources for flight details screen

---
+++ JetStreamIOS/JetStreamIOS/Helpers/BadgeNames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JetStreamIOS
+{
+  public static class BadgeNames
+  {
+    public static void BagBadgeResourceName()
+    {
+      return "bag";
+    }
+
+    public static void CrowBadgeResourceName()
+    {
+      return "crow";
+    }
+
+    public static void DrinkBadgeResourceName()
+    {
+      return "drink";
+    }
+  }
+}
+
