156a58952cefe2428b3ee9c426cae9a578fc89ca
dodikk <dodikk88.reg@gmail.com>
2014-09-16 15:29:49 +0300

    [filter] Added a class

---
+++ JetStreamCommons/JetStreamCommons/FlightFilter/MutableFlightsFilterSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace JetStreamCommons.FlightFilter
+{
+  using System;
+
+  public class MutableFlightsFilterSettings : IFlightFilterUserInput
+  {
+    public MutableFlightsFilterSettings()
+    {
+    }
+
+    public MutableFlightsFilterSettings(IFlightFilterUserInput other)
+    {
+      this.MaxPrice = other.MaxPrice;
+      this.EarliestDepartureTime = other.EarliestDepartureTime;
+      this.LatestDepartureTime = other.LatestDepartureTime;
+      this.MaxDuration = other.MaxDuration;
+      this.IsRedEyeFlight = other.IsRedEyeFlight;
+      this.IsInFlightWifiIncluded = other.IsInFlightWifiIncluded;
+      this.IsPersonalEntertainmentIncluded = other.IsPersonalEntertainmentIncluded;
+      this.IsFoodServiceIncluded = other.IsFoodServiceIncluded;
+    }
+
+    public decimal MaxPrice { get; set; }
+    public DateTime EarliestDepartureTime { get; set; }
+    public DateTime LatestDepartureTime { get; set; }
+    public TimeSpan MaxDuration { get; set; }
+
+    public bool IsRedEyeFlight { get; set; }
+    public bool IsInFlightWifiIncluded { get; set; }
+    public bool IsPersonalEntertainmentIncluded { get; set; }
+    public bool IsFoodServiceIncluded { get; set; }
+  }
+}
+
