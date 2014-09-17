34d091d39bb3f0829d7abb785e3f532b9db995b9
dodikk <dodikk88.reg@gmail.com>
2014-09-17 18:39:28 +0300

    [time] Added item based timezone impl

---
+++ JetStreamCommons/JetStreamCommons/Airport/TimeZoneInfoWithItem.cs
@@ -0,0 +1,41 @@
+﻿namespace JetStreamCommons.Airport
+{
+  using System;
+  using Sitecore.MobileSDK.API.Items;
+
+
+  public class TimeZoneInfoWithItem : ITimeZoneInfo
+  {
+    private ISitecoreItem underlyingItem;
+
+    public TimeZoneInfoWithItem(ISitecoreItem item)
+    {
+      this.underlyingItem = item;
+    }
+
+    public string Name 
+    { 
+      get
+      {
+        return this.underlyingItem["Time Zone Name"].RawValue;
+      }
+    }
+
+    public string Id 
+    { 
+      get
+      {
+        return this.underlyingItem["Time Zone ID"].RawValue;
+      }
+    }
+
+    public string Abbreviation
+    { 
+      get
+      {
+        return this.underlyingItem["Abbreviation"].RawValue;
+      }
+    }
+  }
+}
+
