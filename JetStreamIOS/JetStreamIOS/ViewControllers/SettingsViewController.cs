da578ba476e8737d345dd1222c7f3e18c8e477ac
dodikk <dodikk88.reg@gmail.com>
2014-10-27 18:29:33 +0200

    [deployment] Fixed after packages renaming

--- JetStreamIOS/JetStreamIOS/ViewControllers/SettingsViewController.cs
+++ JetStreamIOS/JetStreamIOS/ViewControllers/SettingsViewController.cs
@@ -10,8 +10,7 @@ namespace JetStreamIOS
   using JetStreamCommons;
   using Sitecore.MobileSDK.API;
   using Sitecore.MobileSDK.API.Session;
-
-  using  SecureStringPasswordProvider.iOS;
+  using Sitecore.MobileSDK.PasswordProvider.iOS;
 
 
 	public partial class SettingsViewController : UIViewController
