060f41752784509403964a82bcf4b272dad5eae4
Igor <igor.kh.mail@gmail.com>
2015-05-21 18:02:28 +0300

    drawing pins as circled images

---
+++ src/ios/JetStreamIOSFull/Helpers/ImageResize.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UIKit;
+using System.Drawing;
+
+namespace JetStreamIOSFull
+{
+  public static class ImageResize
+  {
+    public static UIImage MaxResizeImage(UIImage sourceImage, float maxWidth, float maxHeight)
+    {
+      var sourceSize = sourceImage.Size;
+      var maxResizeFactor = Math.Max(maxWidth / sourceSize.Width, maxHeight / sourceSize.Height);
+      if (maxResizeFactor > 1) return sourceImage;
+      float width = (float)(maxResizeFactor * sourceSize.Width);
+      float height = (float)(maxResizeFactor * sourceSize.Height);
+      UIGraphics.BeginImageContext(new SizeF(width, height));
+      sourceImage.Draw(new RectangleF(0, 0, width, height));
+      var resultImage = UIGraphics.GetImageFromCurrentImageContext();
+      UIGraphics.EndImageContext();
+      return resultImage;
+    }
+
+    public static UIImage ResizeImage(UIImage sourceImage, float width, float height)
+    {
+      UIGraphics.BeginImageContext(new SizeF(width, height));
+      sourceImage.Draw(new RectangleF(0, 0, width, height));
+      var resultImage = UIGraphics.GetImageFromCurrentImageContext();
+      UIGraphics.EndImageContext();
+      return resultImage;
+    }
+
+    private static UIImage CropImage(UIImage sourceImage, int crop_x, int crop_y, int width, int height)
+    {
+      var imgSize = sourceImage.Size;
+      UIGraphics.BeginImageContext(new SizeF(width, height));
+      var context = UIGraphics.GetCurrentContext();
+      var clippedRect = new RectangleF(0, 0, width, height);
+      context.ClipToRect(clippedRect);
+      float fwidth = (float)imgSize.Width;
+      float fheight = (float)imgSize.Height;
+      var drawRect = new RectangleF(-crop_x, -crop_y, fwidth, fheight);
+      sourceImage.Draw(drawRect);
+      var modifiedImage = UIGraphics.GetImageFromCurrentImageContext();
+      UIGraphics.EndImageContext();
+      return modifiedImage;
+    }
+  }
+}
+
--- src/ios/JetStreamIOSFull/MapDelegate.cs
+++ src/ios/JetStreamIOSFull/MapDelegate.cs
@@ -16,30 +16,15 @@ namespace JetStreamIOSFull
 
     public override MKAnnotationView GetViewForAnnotation(MKMapView mapView, IMKAnnotation annotation)
     {
-      // try and dequeue the annotation view
       MKAnnotationView annotationView = mapView.DequeueReusableAnnotation(annotationIdentifier);   
-      // if we couldn't dequeue one, create a new one
       if (annotationView == null)
         annotationView = new MKPinAnnotationView(annotation, annotationIdentifier);
-      else // if we did dequeue one for reuse, assign the annotation to it
+      else 
         annotationView.Annotation = annotation;
 
-      // configure our annotation view properties
-      annotationView.CanShowCallout = true;
+      annotationView.CanShowCallout = false;
       (annotationView as MKPinAnnotationView).AnimatesDrop = true;
-      (annotationView as MKPinAnnotationView).PinColor = MKPinAnnotationColor.Green;
-      annotationView.Selected = true;
 
-      // you can add an accessory view; in this case, a button on the right and an image on the left
-      detailButton = UIButton.FromType(UIButtonType.DetailDisclosure);
-      detailButton.TouchUpInside += (s, e) => {
-        Console.WriteLine ("Clicked");
-        new UIAlertView("Annotation Clicked", "You clicked on " +
-          (annotation as MKAnnotation).Coordinate.Latitude.ToString() + ", " +
-          (annotation as MKAnnotation).Coordinate.Longitude.ToString() , null, "OK", null).Show();
-      };
-
-      annotationView.RightCalloutAccessoryView = detailButton;
 
       string instanceUrl = this.endpoint.InstanceUrl;
       string imagePath = annotation.GetTitle();
@@ -55,11 +40,12 @@ namespace JetStreamIOSFull
 
         if (image != null && finished) {
           InvokeOnMainThread ( () => {
+            UIImage resizedImage = ImageResize.ResizeImage(image, 100, 100);  
             CALayer imageLayer = annotationView.Layer;
-            imageLayer.CornerRadius = image.Size.Height/2;
+            imageLayer.CornerRadius = resizedImage.Size.Height/2;
             imageLayer.BorderWidth = 1;
             imageLayer.MasksToBounds = true;
-            annotationView.Image = image;// = new UIImageView(image);          
+            annotationView.Image = resizedImage;
           });
         }
 
@@ -67,9 +53,8 @@ namespace JetStreamIOSFull
       );
       return annotationView;
     }     
+     
 
-    // as an optimization, you should override this method to add or remove annotations as the
-    // map zooms in or out.
     public override void RegionChanged (MKMapView mapView, bool animated) {}
   }
 }
