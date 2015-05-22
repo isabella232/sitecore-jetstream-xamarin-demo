2eb71524736e2ffe93105379c63342afe02781e2
Igor <igor.kh.mail@gmail.com>
2015-05-22 11:48:37 +0300

    project localisation added

--- src/ios/JetStreamIOSFull/DetailViewController.cs
+++ src/ios/JetStreamIOSFull/DetailViewController.cs
@@ -13,7 +13,6 @@ namespace JetStreamIOSFull
 {
   public partial class DetailViewController : UIViewController, IMKMapViewDelegate
   {
-    public object DetailItem { get; set; }
     private IEnumerable destinations;
     private InstanceSettings.InstanceSettings endpoint;
 
@@ -23,30 +22,15 @@ namespace JetStreamIOSFull
 
     public void SetDetailItem(object newDetailItem)
     {
-      if (DetailItem != newDetailItem)
-      {
-        DetailItem = newDetailItem;
-        
-        // Update the view
-        ConfigureView();
-      }
-    }
-
-    void ConfigureView()
-    {
-      // Update the user interface for the detail item
-      if (IsViewLoaded && DetailItem != null)
-        detailDescriptionLabel.Text = DetailItem.ToString();
     }
 
     public async override void ViewDidLoad()
     {
       base.ViewDidLoad();
-      // Perform any additional setup after loading the view, typically from a nib.
-      ConfigureView();
 
       this.destinations = await this.DownloadAllDestinations();
       this.RefreshMap();
+
       MapDelegate mapDelegate = new MapDelegate();
       mapDelegate.endpoint = this.endpoint;
       this.map.Delegate = mapDelegate;
@@ -78,24 +62,21 @@ namespace JetStreamIOSFull
     {
       try
       {
-        // It will automatically get values from the NSUserDefaults singleton
+        //FIXME: error here, some objects must be disposed!!!
         this.endpoint = new InstanceSettings.InstanceSettings();
 
         var session = this.endpoint.GetSession();
         using (var loader = new DestinationsLoader(session))
         {
-          return await loader.LoadOnlyDestionations();
+          return await loader.LoadOnlyDestinations();
         }
       }
       catch
       {
-        AlertHelper.ShowLocalizedAlertWithOkOption("AIRPORTS_DOWNLOAD_FAILURE_ALERT_TITLE", "AIRPORTS_DOWNLOAD_FAILURE_ALERT_MESSAGE");
+        AlertHelper.ShowLocalizedAlertWithOkOption("NETWORK_ERROR_TITLE", "CANNOT_DOWNLOAD_DESTINATIONS_ERROR");
         return null;
       }
-      finally
-      {
-        //this.HideLoader();
-      }
+
     }
 
     public override void DidReceiveMemoryWarning()
