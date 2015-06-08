c67b6a89bc8a9183498fdf94b4b2dcefb7f23812
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-06-08 16:25:45 +0300

    [ADD] Added scroll view library for smooth animations.

---
+++ src/android/LibraryProjects/Xamarin.Android.ObservableScrollView/ITouchInterceptionListener.cs
@@ -0,0 +1,43 @@
+﻿
+using MotionEvent = Android.Views.MotionEvent;
+
+namespace Xamarin.Android.ObservableScrollView
+{
+  public interface ITouchInterceptionListener
+	{
+		/**
+         * Determines whether the layout should intercept this event.
+         *
+         * @param ev     motion event
+         * @param moving true if this event is ACTION_MOVE type
+         * @param diffX  difference between previous X and current X, if moving is true
+         * @param diffY  difference between previous Y and current Y, if moving is true
+         * @return true if the layout should intercept
+         */
+		bool ShouldInterceptTouchEvent(MotionEvent ev, bool moving, float diffX, float diffY);
+
+		/**
+         * Called if the down motion event is intercepted by this layout.
+         *
+         * @param ev motion event
+         */
+		void OnDownMotionEvent(MotionEvent ev);
+
+		/**
+         * Called if the move motion event is intercepted by this layout.
+         *
+         * @param ev    motion event
+         * @param diffX difference between previous X and current X
+         * @param diffY difference between previous Y and current Y
+         */
+		void OnMoveMotionEvent(MotionEvent ev, float diffX, float diffY);
+
+		/**
+         * Called if the up (or cancel) motion event is intercepted by this layout.
+         *
+         * @param ev motion event
+         */
+		void OnUpOrCancelMotionEvent(MotionEvent ev);
+	}
+}
+
---
+++ src/android/LibraryProjects/Xamarin.Android.ObservableScrollView/ScrollState.cs
@@ -0,0 +1,22 @@
+﻿namespace Xamarin.Android.ObservableScrollView
+{
+  public enum ScrollState
+	{
+		/**
+     * Widget is stopped.
+     * This state does not always mean that this widget have never been scrolled.
+     */
+		Stop,
+
+		/**
+     * Widget is scrolled up by swiping it down.
+     */
+		Up,
+
+		/**
+     * Widget is scrolled down by swiping it up.
+     */
+		Down,
+	}
+}
+
