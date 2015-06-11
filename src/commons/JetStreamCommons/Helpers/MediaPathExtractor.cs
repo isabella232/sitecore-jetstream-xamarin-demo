3958825386433edbc25b47742d2d5abe3e7fcbc5
Igor <igor.kh.mail@gmail.com>
2015-06-11 11:16:04 +0300

    media path building fix

--- src/commons/JetStreamCommons/Helpers/MediaPathExtractor.cs
+++ src/commons/JetStreamCommons/Helpers/MediaPathExtractor.cs
@@ -7,9 +7,17 @@ namespace JetStreamCommons
     public static string GetImagePathFromImageRawValue(string rawValue)
     {
       //<image src="~/media/605015FD900C488AAD0E8F3762F42A43.ashx" mediaid="{605015FD-900C-488A-AD0E-8F3762F42A43}" mediapath="/Images/Imported/8/0/b/6/a_tokyooverviewFEB09" />
-     
+      //<image mediaid="{903A0864-B02F-44CD-9DEB-45CAF0FE08A3}" mediapath="/Images/Planes/14061" src="~/media/903A0864B02F44CD9DEB45CAF0FE08A3.ashx" />
+
+      string prefixToSearch = "src=\"";
+      string postfixToSearch = ".ashx";
+
+      int srcIndex = rawValue.IndexOf(prefixToSearch) + prefixToSearch.Length;
+      int finishIndex = rawValue.IndexOf(postfixToSearch) + postfixToSearch.Length;
+      int mediaPathLenght = finishIndex - srcIndex;
+
       //TODO: igk magik numbers, must be ok, but possible error
-      return rawValue.Substring(12, 45);
+      return rawValue.Substring(srcIndex, mediaPathLenght);
     }
   }
 }
