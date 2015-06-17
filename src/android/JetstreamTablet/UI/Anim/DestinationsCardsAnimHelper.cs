a31a1a71f78152357c9f591d60140fbdf21cf034
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-06-17 14:01:50 +0300

    [FIX] Fixed bug with animation.

--- src/android/JetstreamTablet/UI/Anim/DestinationsCardsAnimHelper.cs
+++ src/android/JetstreamTablet/UI/Anim/DestinationsCardsAnimHelper.cs
@@ -13,42 +13,13 @@
       PrepareAppearanceAnimation(layout).Start();
     }
 
-    public static void AnimateDisappearance(View layout)
-    {
-      PrepareDisappearanceAnimation(layout).Start();
-    }
-
-    private static AnimatorSet PrepareDisappearanceAnimation(View layout)
-    {
-      ViewGroup parent = (ViewGroup)layout.Parent;
-      int distance = parent.Height - layout.Top;
-
-      var set = new AnimatorSet();
-      set.PlayTogether(
-        ObjectAnimator.OfFloat(layout, "alpha", 1, 0),
-        ObjectAnimator.OfFloat(layout, "translationY", 0, distance));
-
-      set.SetDuration(AnimationDuration);
-      set.SetTarget(layout);
-
-      var listener = new AnimatorListenerImpl
-        {
-          AnimationEnd = animator => layout.Visibility = ViewStates.Gone
-        };
-
-      set.AddListener(listener);
-      return set;
-    }
-
     private static AnimatorSet PrepareAppearanceAnimation(View layout)
     {
       ViewGroup parent = (ViewGroup)layout.Parent;
       int distance = parent.MeasuredHeight - layout.Top;
 
       var set = new AnimatorSet();
-      set.PlayTogether(
-        ObjectAnimator.OfFloat(layout, "alpha", 0, 1),
-        ObjectAnimator.OfFloat(layout, "translationY", distance, 0));
+      set.Play(ObjectAnimator.OfFloat(layout, "translationY", distance, 0));
 
       set.SetDuration(AnimationDuration);
       set.SetTarget(layout);
